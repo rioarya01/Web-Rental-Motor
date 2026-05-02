dcaf17abf32f2215f92b59433101ec97a94a6904
rioarya01 <rio283931@gmail.com>
2026-05-02 18:27:31 +0800

    feat: add terms and conditions component

---
+++ resources/views/components/terms-conditions.blade.php
@@ -0,0 +1,32 @@
+<div class="space-y-4 text-justify">
+    <!-- Judul -->
+    <div>
+        <h2 class="text-2xl font-bold text-gray-900">
+            Syarat & Ketentuan Rental Motor
+        </h2>
+        <p class="text-md text-gray-600 mt-3">
+            Transparansi adalah kunci. Pastikan Anda memenuhi beberapa persyaratan dasar sebelum melakukan pemesanan.
+        </p>
+    </div>
+    <!-- Isi -->
+    <ol class="list-decimal pl-5 space-y-3 text-md text-gray-700">
+        <li>
+            Lakukan pemesanan minimal 1x24 jam sebelum waktu penggunaan. Setelah pesanan dikonfirmasi, penyewa wajib melakukan konfirmasi pengambilan motor melalui nomor WhatsApp atau datang ke lokasi Sewa Motor ID.
+        </li>
+        <li>
+            Membawa <b>2 kartu identitas</b> (SIM A, KK, KTP, ID Kerja atau ID Pelajar).
+        </li>
+        <li>
+            Menyertakan akun sosial media dan <b>nomor whatsApp</b> yang aktif.
+        </li>
+        <li>
+            Pada saat serah terima kendaraan, pihak penyedia rental akan melakukan <b>pengecekan keaslian data</b> dan kondisi kendaraan.
+        </li>
+        <li>
+            Pihak penyedia rental <b>berhak membatalkan</b> apabila data tidak sesuai dan syarat sewa tidak dapat dilengkapi.
+        </li>
+        <li>
+            Apabila pembayaran telah selesai dan kendaraan yang dipesan tidak tersedia, penyewa <b>berhak memilih kendaraan lain</b> yang tersedia tanpa dikenakan biaya tambahan.
+        </li>
+    </ol>
+</div>
