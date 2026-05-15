6fdbfbcb4e9ead40e6541df9907b416c45c1bc68
rioarya01 <rio283931@gmail.com>
2026-05-15 21:47:52 +0800

    feat(layout): add footer section

--- resources/views/layouts/footerUser.blade.php
+++ resources/views/layouts/footerUser.blade.php
@@ -1,32 +1,39 @@
-<footer class="bg-dark text-white py-4 mt-5">
+<footer class="bg-dark text-white py-5 mt-5">
     <div class="container">
-        <div class="row">
-            <div class="col-md-4 mb-3">
-                <h5>About Us</h5>
-                <p>Your one-stop shop for quality products and excellent service. Thank you for visiting!</p>
+        <div class="row gy-4">
+            <div class="col-md-8">
+                <!-- Logo -->
+                <a href="{{ route('home.user') }}" class="navbar-brand d-flex align-items-center mb-3">
+                    <img 
+                        src="{{ asset('img/logo/horizontal-white.svg') }}" 
+                        alt="Logo"
+                        class="img-fluid"
+                        style="height: 60px; width: auto;"
+                    >
+                </a>
+                <p class="mb-0">Pilihan terlengkap, harga transparan, dan layanan 24 jam. Siap menemani setiap petualangan Anda.</p>
             </div>
-            <div class="col-md-4 mb-3">
-                <h5>Quick Links</h5>
-                <ul class="list-unstyled">
-                    <li><a href="{{ url('/') }}" class="text-white">Home</a></li>
+            <div class="col-6 col-md-2">
+                <h5 class="text-white fw-semibold mb-3">About</h5>
+                <ul class="list-unstyled mb-0">
+                    <li><a href="#" class="text-white text-decoration-none">Chat Admin</a></li>
                 </ul>
             </div>
-            <div class="col-md-4 mb-3">
-                <h5>Contact</h5>
-                <ul class="list-unstyled">
-                    <li>Email: info@automoto.com</li>
-                    <li>Phone: +1 234 567 890</li>
-                    <li>
-                        <a href="#" class="text-white me-2"><i class="fab fa-facebook"></i></a>
-                        <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
-                        <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
-                    </li>
+            <div class="col-6 col-md-2">
+                <h5 class="text-white fw-semibold mb-3">Socials</h5>
+                <ul class="list-unstyled mb-0">
+                    <li><a href="#" class="text-white text-decoration-none">Discord</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Instagram</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Twitter</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Facebook</a></li>
                 </ul>
             </div>
         </div>
-        <hr class="bg-light">
-        <div class="text-center">
-            &copy; {{ date('Y') }} AutoMoto. All rights reserved.
+
+        <hr class="border-secondary my-4">
+
+        <div class="fw-semibold small">
+            &copy; {{ date('Y') }} Sewa Motor ID. All rights reserved.
         </div>
     </div>
 </footer>
