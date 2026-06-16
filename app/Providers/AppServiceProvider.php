add9f8da37eb4ea59a0e5ba0b343addfe7bd226a
rioarya01 <rio283931@gmail.com>
2026-06-17 00:45:39 +0800

    fix: force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
         config(['app.locale' => 'id']);
         \Carbon\Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
